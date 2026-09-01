ebf24212beeefd2744196f2bcae5586c5e94d7fe
Anmol <anmolkkw467@gmail.com>
2026-09-01 21:17:05 +0530

    Connect frontend to deployed backend

--- AutoCare-Frontend/car-service-frontend/vite.config.js
+++ AutoCare-Frontend/car-service-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5019",
+        target: "https://car-service-frontend-nu.vercel.app/",
         changeOrigin: true
       }
     }
